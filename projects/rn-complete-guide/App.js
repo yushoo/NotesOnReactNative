9ea7214ca0186e99ef8e64c05db1fdbdcdabefe3
Henry Feng <henryfeng12345@gmail.com>
2020-01-06 11:07:28 -0600

    add inline styles for layout

--- projects/rn-complete-guide/App.js
+++ projects/rn-complete-guide/App.js
@@ -3,9 +3,9 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View >
-      <View>
-        <TextInput />
+    <View style={{padding: 50}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput placeholder="Goal" style={{borderBottomColor: 'black', borderBottomWidth: 1, padding: 10, width: 200}}/>
         <Button title="ADD" />
       </View>
       <View>
